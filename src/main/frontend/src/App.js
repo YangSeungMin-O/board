5af048ab0cd8322585be84120c715a9eaf16b4fa
YangSeungMin <101317184+YangSeungMin-O@users.noreply.github.com>
2022-07-05 00:06:00 +0900

    리액트 라우터 설정 및 리액트 제공 함수 사용

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './view/home';
+import About from './view/about';
+import Profile from './view/profile';
+
+  /*Link를 사용하면 A태그와 달리 새로고침이 없어서 부드러운 전환이 가능하다*/
+  /*JS에서 Export할때는 항상 첫글자가 대문자여야 오류가 발생하지 않는다*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
+    <Link to='/profile'>Profile</Link>
+  </nav>
+    <header>----------------------------------</header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    <footer>----------------------------------</footer>
+  </BrowserRouter>
   );
 }
 
