9e980994df79bc82075c7f69ebb514cd95a7430f
YangSeungMin <ysm6768@sodasys.com>
2022-07-13 18:40:37 +0900

    게시판 CSS적용 게시판 DATA소스 수정중

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -11,6 +11,9 @@ import Axios from './view/test/axios';
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/loginPage" element={<Login />} />
--- src/main/frontend/src/view/main/main.js
+++ src/main/frontend/src/view/main/main.js
@@ -1,90 +1,110 @@
 import { useEffect, useState } from 'react';
+import { ArchiveFill, MicFill, CameraVideo, Inboxes, Bell, PersonCircle } from 'react-bootstrap-icons';
 import axios from "axios";
-//import * as main2 from "./main2"
 
 export default function Main() {
-    /* 데이터 리스트 */
-    const [list, setList] = useState([]);
-    /* 페이지 번호 */
-    const [page, setPage] = useState(1);
-    /* 페이지 번호 총 갯수 */
-    const [totalPage, setTotalPage] = useState([]);
-    /* 페이지 전환 시 */
-    const pageChange = (event) => {
-      setPage(event.target.value);
-    }
-    /* 검색키워드 */
-    const [searchKey, setSearchKey] = useState([]);
-    /* 검색키워드 변경 시 실시간 반영 */
-    const searchKeyChange = (event) => {
-      setSearchKey(event.target.value);
-    }
-    /* 맨처음 리스트 불러오기 */
-    useEffect(() => {
-      getData();
-    }, [page]);
+/* JS ---------------------------------------------------------------- */
+  const [list, setList] = useState([]);
 
-    function getData(searchKey) {
-      const url = "/api/boardData";
-      const url2 = "/api/boardDataCnt";
-      const result = [];
-      axios.all([axios.get(url,{params : { searchKey: searchKey, page: page }}), axios.get(url2,{params : { searchKey: searchKey, page: page }})])
-      .then(
-        axios.spread((response, response2) => {
-          const {data} = response;
-          setList(data);
-          setTotalPage(Math.ceil(response2.data/10));
-        })
-      )
-      .catch(function(error) {
-        console.log("실패");
-        alert("데이터를 가져오는데 실패하였습니다")
-      })
-      return result;
-    }
+  useEffect(() => {
+    getData();
+  }, []);
 
-   function searchData() {
-      console.log("검색 시행");
-      getData(searchKey)
-   }
-
-   function createPageNumber() {
-      var pageArr = [];
-      for (let i = 0; i < totalPage; i++) {
-        pageArr.push(<button key={i+1} value={i+1} onClick={pageChange}>{i+1}</button>);
-      }
-      return pageArr;
-   }
-
+  const getData = () => {
+    console.log("Hello World!")
+  }
+/* CSS --------------------------------------------------------------- */
+  const pageNavStyle = {
+    float: 'left'
+  }
+  const insertBtnStyle = {
+    float: 'right'
+  }
+  const searchInputStyle = {
+    float: 'right',
+    width: '15%'
+  }
   return (
     <div>
-      <h1>Board Main</h1>
-      <input type="text" placeholder="검색어를 입력하세요" onChange={searchKeyChange}/>
-      <button type="button" onClick={searchData}>검색</button>
-      <table border="1">
-        <thead>
-        <tr>
-          <td>번호</td>
-          <td>제목</td>
-          <td>작성자</td>
-          <td>작성일</td>
-          <td>수정일</td>
-        </tr>
-        </thead>
-        <tbody>
-          {list.map((item) => (
-            <tr key={item.board_no}>
-              <td>{item.board_no}</td>
-              <td>{item.board_title}</td>
-              <td>{item.board_writer}</td>
-              <td>{item.board_reg_date}</td>
-              <td>{item.board_chg_date}</td>
+      <div className="container">
+        <br/>
+        <h1>Board</h1>
+        <input type="text" className="form-control" style={searchInputStyle} placeholder="검색어 입력"></input>
+        <table className="table table-hover text-center">
+          <thead>
+            <tr>
+              <td>번호</td>
+              <td>제목</td>
+              <td>작성자</td>
+              <td>작성일</td>
+              <td>수정일</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div>
-        {createPageNumber()}
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+          </tbody>
+        </table>
+        <div style={pageNavStyle}>
+          <nav aria-label="Page navigation example">
+            <ul className="pagination">
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </a>
+              </li>
+              <li className="page-item"><a className="page-link" href="#">1</a></li>
+              <li className="page-item"><a className="page-link" href="#">2</a></li>
+              <li className="page-item"><a className="page-link" href="#">3</a></li>
+              <li className="page-item">
+                <a className="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <button type="button" className="btn btn-success" style={insertBtnStyle}>게시물 등록</button>
       </div>
     </div>
   )
