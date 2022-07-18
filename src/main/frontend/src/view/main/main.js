6ca329e58f8b92d36cd9f463342863f3d7ad352f
YangSeungMin <ysm6768@sodasys.com>
2022-07-18 11:59:54 +0900

    Test

--- src/main/frontend/src/view/main/main.js
+++ src/main/frontend/src/view/main/main.js
@@ -1,35 +1,64 @@
 import { useEffect, useState } from 'react';
-import { ArchiveFill, MicFill, CameraVideo, Inboxes, Bell, PersonCircle } from 'react-bootstrap-icons';
+import { Search } from 'react-bootstrap-icons';
 import axios from "axios";
 
 export default function Main() {
 /* JS ---------------------------------------------------------------- */
   const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchKey, setSearchKey] = useState("");
+  const searchKeyChange = (event) => {
+    setSearchKey(event.target.value);
+  };
 
   useEffect(() => {
     getData();
   }, []);
 
-  const getData = () => {
-    console.log("Hello World!")
+  function getData() {
+    const url = "/api/boardData";
+    axios.get(url, {params: {searchKey: searchKey, page: page}})
+    .then(function(response) {
+        console.log("성공");
+        const {data} = response;
+        setList(data);
+    })
+    .catch(function(error) {
+        console.log("실패");
+        console.log(error);
+    })
+  }
+
+  function getTotalPage() {
+
+  }
+
+  function searchData() {
+      console.log("검색 시행");
+      getData(searchKey)
   }
 /* CSS --------------------------------------------------------------- */
   const pageNavStyle = {
-    float: 'left'
+
   }
   const insertBtnStyle = {
+    display: 'inline-block',
     float: 'right'
   }
   const searchInputStyle = {
     float: 'right',
-    width: '15%'
+    width: '25%'
+  }
+  const searchBtnStyle = {
+      float: 'right'
   }
   return (
     <div>
       <div className="container">
         <br/>
         <h1>Board</h1>
-        <input type="text" className="form-control" style={searchInputStyle} placeholder="검색어 입력"></input>
+        <button type="button" className="btn btn-success" style={searchBtnStyle} onClick={searchData}>검색</button>
+        <input type="text" className="form-control" style={searchInputStyle} placeholder="검색어 입력" onChange={searchKeyChange}></input>
         <table className="table table-hover text-center">
           <thead>
             <tr>
@@ -41,53 +70,20 @@ export default function Main() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>2</td>
-              <td>3</td>
-              <td>4</td>
-              <td>5</td>
-            </tr>
+            {list.map((item) => (
+              <tr key={item.board_no}>
+                <td>{item.board_no}</td>
+                <td>{item.board_title}</td>
+                <td>{item.board_writer}</td>
+                <td>{item.board_reg_date}</td>
+                <td>{item.board_chg_date}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <div style={pageNavStyle}>
           <nav aria-label="Page navigation example">
-            <ul className="pagination">
+            <ul className="pagination justify-content-center">
               <li className="page-item">
                 <a className="page-link" href="#" aria-label="Previous">
                   <span aria-hidden="true">&laquo;</span>
@@ -104,7 +100,9 @@ export default function Main() {
             </ul>
           </nav>
         </div>
-        <button type="button" className="btn btn-success" style={insertBtnStyle}>게시물 등록</button>
+        <div style={insertBtnStyle}>
+          <button type="button" className="btn btn-success">게시물 등록</button>
+        </div>
       </div>
     </div>
   )
