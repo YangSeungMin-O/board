e5bcf5cdd115f9b57b88067a0e11d747e6e09835
YangSeungMin <ysm6768@sodasys.com>
2022-07-22 12:05:42 +0900

    메이븐추가 및 @Value테스트 중 스캔을 못하는걸로 예상됨

--- src/main/java/co/kr/board/security/config/JwtTokenProvider.java
+++ src/main/java/co/kr/board/security/config/JwtTokenProvider.java
@@ -1,31 +1,26 @@
 package co.kr.board.security.config;
 
-import co.kr.board.security.service.CustomUserDetailsService;
-import co.kr.board.security.service.impl.SecurityMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-  /*@Value("${secretKey}")*/
+  @Value("${secretKey}")
+  private String testKey;
+
   private String secretKey = "jsdfgneuirvnuiefbvihbfhsdbvhjfsdbvyusekryvbfskdjfvhbsyvukrehjbsfvyuskhdbchdsj";
   public String createToken(String username, String userAuth) {
     Long expiredTime = 1000 * 60L * 60L * 2L; // 토큰 유효 시간 (2시간)
     Date ext = new Date(); // 토큰 만료 시간
     ext.setTime(ext.getTime() + expiredTime);
+
+    System.out.println(testKey);
 
     String token = Jwts.builder()
         .claim("username", username) //토큰에 들어갈 정보 1
